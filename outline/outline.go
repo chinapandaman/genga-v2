6b0f4d8e578d4e5af63eb0a7420900253a3926b9
chinapandaman <benjaminli801@gmail.com>
2026-06-13 21:22:27 -0500

    finished outline logics

--- outline/outline.go
+++ outline/outline.go
@@ -1,12 +1,14 @@
 package outline
 
 import (
-	"fmt"
-	"os"
 	"image"
+	"image/color"
 	_ "image/jpeg"
+	"image/png"
 	_ "image/png"
 	"log"
+	"math"
+	"os"
 )
 
 func getEightBitRGB(r16 uint32, g16 uint32, b16 uint32) (uint8, uint8, uint8) {
@@ -29,9 +31,35 @@ func OutlineImage(path string) {
 		log.Fatalf("Failed to decode image: %v", err)
 	}
 
-	x, y := 10, 20
-	r16, g16, b16, _ := img.At(x, y).RGBA()
+	width := img.Bounds().Dx()
+	height := img.Bounds().Dy()
 
-	r8, g8, b8 := getEightBitRGB(r16, g16, b16)
-	fmt.Println(r8, g8, b8)
+	newBounds := image.Rect(0, 0, width, height)
+	newImg := image.NewRGBA(newBounds)
+
+	for x := 1; x <= width; x++ {
+		for y := 1; y <= height; y++ {
+			r16, g16, b16, _ := img.At(x, y).RGBA()
+			r8, g8, b8 := getEightBitRGB(r16, g16, b16)
+
+			r16, g16, b16, _ = img.At(x-1, y-1).RGBA()
+			r82, g82, b82 := getEightBitRGB(r16, g16, b16)
+
+			diff := math.Sqrt(float64((r8-r82)*(r8-r82) + (g8-g82)*(g8-g82) + (b8-b82)*(b8-b82)))
+			if diff > 5 {
+				black := color.RGBA{R: 0, G: 0, B: 0, A: 255}
+				newImg.Set(x, y, black)
+			}
+		}
+	}
+
+	output, err := os.Create("./images/output.png")
+	if err != nil {
+		log.Fatalf("Failed to create outlined image file: %v", err)
+	}
+	defer output.Close()
+
+	if err := png.Encode(output, newImg); err != nil {
+		log.Fatalf("Failed to save outlined image: %v", err)
+	}
 }
